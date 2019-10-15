d6c0a2aa2f3e824903c713b1088f8868d0a3f975
damon <717474983@qq.com>
2019-10-16 00:21:43 +0800

    --

--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/router";
 import "./registerServiceWorker";
-// import "amfe-flexible";
 
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 Vue.use(ElementUI);
 
 // import { Style } from "cube-ui";
--- src/views/Report.vue
+++ src/views/Report.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="main">
-    123
+    <el-card class="box-card">
+      <el-button @click="exportData">导出</el-button>
+      <el-table :data="tableData" border style="width: 100%;margin: 10px 0;">
+        <el-table-column prop="date" label="日期" width="180"></el-table-column>
+        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+      </el-table>
+      <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+    </el-card>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "home",
   components: {},
@@ -13,16 +20,36 @@ export default {
     return {
       pageNum: 1,
       pageSize: 5,
-      articleList: []
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ]
     };
   },
   mounted() {
     this.getArticle();
   },
   methods: {
-    getArticle(){
-
-    }
+    getArticle() {},
+    exportData() {}
   }
 };
 </script>
